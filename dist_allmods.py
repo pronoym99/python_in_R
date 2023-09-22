6fdb9ec8ea11d83336653d7b701e9d339147b0bc
Manjima Dutta <manjimadutta1998@gmail.com>
2023-09-22 14:31:16 +0530

    print_eda function addition

--- dist_allmods.py
+++ dist_allmods.py
@@ -261,7 +261,6 @@ def dist_allmods(i):
 
     return ff
 
-
 # ign = pd.read_csv('data/dtignmast.csv', parse_dates=['strt','end'])
 
 
@@ -315,6 +314,25 @@ def select_ign_time(row):
         return row['ign_time_cst']
     else:
         return row['ign_time_ignMaster']
+    
+def print_eda(datam):
+    datam['date'] = datam['start_time'].dt.date
+    start_time = pd.to_datetime('22:00:00').time()
+    datam['date'] = datam.apply(lambda row: row['date'] if start_time > row['start_time'].time() else (row['start_time'] + pd.DateOffset(days=1)).date(), axis=1)
+    datam['tottime_move'] = datam.apply(lambda row: row['total_time'] if row['status']=='movement' else 0,axis=1)
+    datam['tottime_stop'] = datam.apply(lambda row: row['total_time'] if row['status']=='stationary' else 0,axis=1)
+    datam['tottime_stop_ign_on'] = datam.apply(lambda row: row['final_ign_time'] if row['status']=='stationary' else 0,axis=1)
+    datam['totdist_move'] = datam.apply(lambda row: row['total_dist'] if row['status']=='movement' else 0,axis=1)
+    print_eda=ch.groupby(['reg_numb','date']).agg({'totdist_move':'sum','total_time':'sum','final_ign_time':'sum','tottime_move':'sum','tottime_stop':'sum','tottime_stop_ign_on':'sum'}).reset_index()
+    print_eda.rename(columns={'date':'date1','totdist_move':'km_dist','total_time':'hours_timespan','final_ign_time':'hours_ign_on','tottime_move':'hours_move','tottime_stop':'hours_idle','tottime_stop_ign_on':'hours_idle_on'},inplace=True)
+    print_eda['km_dist'] = print_eda['km_dist']/1000
+    print_eda[['hours_timespan','hours_ign_on','hours_move','hours_idle','hours_idle_on']] /=60
+    print_eda['hours_idle_off'] = print_eda['hours_idle']-print_eda['hours_idle_on']
+    print_eda['idle_pct'] = print_eda['hours_idle']/print_eda['hours_timespan']
+    print_eda['idleon_pct'] = print_eda['hours_idle_on']/print_eda['hours_idle']
+    print_eda['mach_util_pct'] = print_eda['hours_ign_on']/print_eda['hours_timespan']
+    return print_eda
+
 
 if __name__ == '__main__':
     # num_cores = cpu_count()
@@ -352,6 +370,7 @@ if __name__ == '__main__':
       ign['strt'] = pd.to_datetime(ign['IgnON'], unit='s', utc=True).dt.tz_convert('Asia/Kolkata').dt.tz_localize(None)
       ign['end'] = pd.to_datetime(ign['IgnOFF'], unit='s', utc=True).dt.tz_convert('Asia/Kolkata').dt.tz_localize(None)
       ign['termid'] = ign['termid'].astype(int)
+    #   dist_allmods(1204000258)
       final_df = pd.concat([dist_allmods(termid) for termid in tqdm(termid_list)])
       final_df_dict=final_df.to_dict('records')
       integrated_df = pd.concat([ign_time_int(termid) for termid in tqdm(termid_list)])
@@ -361,6 +380,7 @@ if __name__ == '__main__':
       integrated_df=integrated_df.reset_index(drop=True)
       integrated_df = final_data_f(integrated_df)
       integrated_df['final_ign_time'] = integrated_df.apply(select_ign_time, axis=1)
+      print_eda_df = print_eda(integrated_df)
       integrated_df['start_time'] = (integrated_df['start_time'] - pd.Timestamp("1970-01-01 05:30:00")) // pd.Timedelta('1s')
       integrated_df['end_time'] = (integrated_df['end_time'] - pd.Timestamp("1970-01-01 05:30:00")) // pd.Timedelta('1s')
       if 'b_sl' in integrated_df.columns:
@@ -368,17 +388,20 @@ if __name__ == '__main__':
 
       if len(sys.argv) == 3:
         integrated_df.to_csv('Integrated_dist_allmods.csv')
+        print_eda_df.to_csv('print_eda.csv')
         print('Data saved successfully to the above path')
 
       # Check whether the last arg is appropriate
-      elif len(sys.argv) == 4:
-        outfile = Path(sys.argv[3])
-        if outfile.suffix != '.csv':
-          print('Need to write output to a CSV file only\nExiting....')
+      elif len(sys.argv) == 5:
+        outfile1 = Path(sys.argv[3])
+        outfile2 = Path(sys.argv[4])
+        if (outfile1.suffix != '.csv')or(outfile2.suffix != '.csv'):
+          print('Need to write both outputs to a CSV file only\nExiting....')
           sys.exit(0)
-        integrated_df.to_csv(outfile)
-        print(f'Data saved successfully to {outfile}')
+        integrated_df.to_csv(outfile1)
+        print_eda_df.to_csv(outfile2)
+        print(f'Outputs saved successfully to below paths \n {outfile1} & {outfile2}')
 
       # Check for extra args
       else:
-        print('Supports atleast 2 and atmost 3 file arguments')
+        print('Supports atleast 2 and atmost 4 file arguments')
